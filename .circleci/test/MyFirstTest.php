10e30bc4d354c3828aeef8ba6c8907a58483637a
birman <birman.cardona@newshore.es>
2020-04-12 10:23:39 -0500

    Add my firts Test

---
+++ .circleci/test/MyFirstTest.php
@@ -0,0 +1,9 @@
+use PHPUnit\Framework\TestCase;
+
+class MyFirstTest extends TestCase
+{
+    public function testHelloWorld()
+    {
+        $this->assertEquals("hello world", "hello world");
+    }
+}
